3fc2c3ac956c6f725734a001c6b10d6f80f122bb
mahendrasonawanegit <96369043+mahendrasonawanegit@users.noreply.github.com>
2021-12-19 13:59:10 +0530

    print statement added

--- Sanity_Rotate_p.py
+++ Sanity_Rotate_p.py
@@ -40,6 +40,7 @@ class SanityTest:
         json_flip_on = {"action": "set", "properties": {"flip": True}, "resource": "cameras/" + self.cam1_cfg['sn']}
         result, data = self.utils.bs_api.shell.send_vz_cmd(json_flip_on)
         result, data = self.utils.check_4_string(str(data), '\'flip\': True')
+        print(result, data)
 
 
         # save logs to csv file is required
